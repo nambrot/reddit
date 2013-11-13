fe89d86b415e065cc646ce9fb94ca755cbb89a19
Nam Chu Hoai <nambrot@googlemail.com>
2013-11-12 20:03:49 -0500

    Point root to Submission Index
    Going to the root results in an error since we haven't defined it.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Reddit::Application.routes.draw do
   resources :submissions
-
+  root 'submissions#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
