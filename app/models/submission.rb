7a4a120329472be84101bcf2ec4fd8c8e5c72095
Nam Chu Hoai <nambrot@googlemail.com>
2013-11-12 19:43:11 -0500

    Validate Submission Attributes
    We don't really want our users to submit titles nor do we want them
    to submit links that are not links, so we need to validate user input.

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,2 +1,10 @@
 class Submission < ActiveRecord::Base
+  validates :link, format: URI::regexp(%w(http https)), presence: true
+  validates :title, presence: true
+
+  # if we want links to be unique
+  # validates :link, uniqueness: true
+
+  # if we want to constrain the length
+  # validates :title, length: {in: 6..10}
 end
