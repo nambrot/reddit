2d1279a13b22a5f7b5cd8b0e4b11ca667a69a4ee
Nam Chu Hoai <nambrot@googlemail.com>
2013-11-12 20:06:36 -0500

    Remove Submission.votes from the form
    We don't really want users to be able set the votes count, so let's
    remove it from the permitted parameters as well as from the form.

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -69,6 +69,6 @@ class SubmissionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def submission_params
-      params.require(:submission).permit(:link, :title, :votes)
+      params.require(:submission).permit(:link, :title)
     end
 end
